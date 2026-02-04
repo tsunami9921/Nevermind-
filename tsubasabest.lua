e23dafe7b5e435d5b0b8e949d1106782974151a5
tsunami9921 <ebilovekber99@gmail.com>
2026-02-04 14:21:00 +0400

    Create tsubasabest.lua

---
+++ tsubasabest.lua
@@ -0,0 +1,15 @@
+
+local previ 
+game.Players.LocalPlayer.PlayerScript.PlayerModule.KnitServiceCode
+
+local client
+(
+previ.Encode
+
+  if previ 0
+
+    do [86544467]
+
+      -- Tsubasa
+
+      print("\loaded/")
