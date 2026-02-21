4798031ba226328a3eccbe46dab3403f5f82783e
tsunami9921 <ebilovekber99@gmail.com>
2026-02-21 19:04:47 +0400

    Update tsubasabest.lua

--- tsubasabest.lua
+++ tsubasabest.lua
@@ -3,13 +3,11 @@ local previ
 game.Players.LocalPlayer.PlayerScript.PlayerModule.KnitServiceCode
 
 local client
-(
-previ.Encode
 
   if previ 0
 
     do [86544467]
 
       -- Tsubasa
-
-      print("\loaded/")
+  end
+      print("loaded")
