ecfd58ff58ffd8b7ab71fe8cc8854a2800fa57f4
tsunami9921 <ebilovekber99@gmail.com>
2026-06-24 02:48:06 +0400

    Remove unused variables and code in tsubasabest.lua

--- tsubasabest.lua
+++ tsubasabest.lua
@@ -1,13 +1,1 @@
 
-local previ 
-game.Players.LocalPlayer.PlayerScript.PlayerModule.KnitServiceCode
-
-local client
-
-  if previ 0
-
-    do [86544467]
-
-      -- Tsubasa
-  end
-      print("loaded")
